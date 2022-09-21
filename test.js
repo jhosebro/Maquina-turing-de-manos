577a0d7f99427512171a95180b6cbea9b06df9ef
Jhoan Rojas <jhosebro2108@gmail.com>
2022-09-21 17:28:12 -0500

    Maquina de turing terminada

---
+++ test.js
@@ -0,0 +1,13 @@
+const translate = require('./index.js');
+
+test('Esto traduce hello correctamente', () => {
+    expect
+    (translate('👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊'))
+    .toBe('Hello');
+});
+
+test('Esto traduce hello world correctamente', () => {
+    expect
+    (translate('👉👆👆👆👆👆👆👆👆🤜👇👈👆👆👆👆👆👆👆👆👆👉🤛👈👊👉👉👆👉👇🤜👆🤛👆👆👉👆👆👉👆👆👆🤜👉🤜👇👉👆👆👆👈👈👆👆👆👉🤛👈👈🤛👉👇👇👇👇👇👊👉👇👉👆👆👆👊👊👆👆👆👊👉👇👊👈👈👆🤜👉🤜👆👉👆🤛👉👉🤛👈👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👉👊👆👆👆👊👇👇👇👇👇👇👊👇👇👇👇👇👇👇👇👊👉👆👊👉👆👊'))
+    .toBe('Hello World!\n');
+});
